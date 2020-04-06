090a2e12d82d0b74972d11f433745c45515e8cbb
Ashkan-Z9 <59195543+Ashkan-Z9@users.noreply.github.com>
2020-04-06 08:21:21 -0400

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -3,6 +3,7 @@ import platform
 import sys
 import os
 from datetime import datetime
+import time
 
 def ex():
     ex = raw_input("\nPress [ENTER] for Exit...")
@@ -32,12 +33,14 @@ def ScanPorts():
     minute = datetime.now() .minute
     second = datetime.now() .second
 
-    print "-> Scan Started in ", (str(year) + '/' + str(month) + '/' + str(day) +' , ' + str(hour) + ':' + str(minute) + ':' + str(second))
+    print ("-> Scan Started in " +  str(year) + '/' + str(month) + '/' + str(day) +' , ' + str(hour) + ':' + str(minute) + ':' + str(second))
     print "-> Host: " + ip
     print "-> Ports Range: " + str(range1) + ', ' + str(range2) + '\n'
     print "++++++++++++++++++++++++++++++++\n"
+    time.sleep(5)
     range2+=1
     count = 0
+    ports = []
     for x in range (range1, range2):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,14 +57,31 @@ def ScanPorts():
                 print "[+] Port " + str(x) + " Open"
                 print '---------------------\n' 
                 count +=1
+		ports.append(x)
 
             else: 
                 print '[-] ' + str(x) + ' Close!'
-
+    
+    range2-=1
     print "\nScan Completed!\n"
     print "========================="
-    print  '--> ' + str(count) + " Ports Open" + ' <--'
+    print  "--> " + str(count) + " Ports Open " + "in Range " + str(range1) + ", " + str(range2) + " <--"
     print "========================="
+    pop = raw_input("\nPRINT OPEN PORTS?(y/n): ")
+    if pop == 'y' or pop == 'Y':
+	number = 1
+	print "---------------------"
+	for openPorts in ports:
+	    print str(number) + ' - ' + str(openPorts)
+	    number+=1
+	print "---------------------\n"
+
+    elif pop == 'n' or pop == 'N':
+	ex()
+
+    else:
+	print "\nUNKNOWN COMMAND!"
+	
     ex()
 
 def GetIp():
@@ -87,14 +107,33 @@ clear()
 if platform.system() == 'Windows':
     os.system('color a')
 
-print '|                             |'
-print '|          Port Scanner       |'
-print '|           @Ashkan_Z9        |'
-print '|                             |'
-print ' -----------------------------\n\n\n'
+print(
+'''
+  _____   ____  _____ _______    _____  _____          _   _ _   _ ______ _____  
+ |  __ \ / __ \|  __ \__   __|  / ____|/ ____|   /\   | \ | | \ | |  ____|  __ \ 
+ | |__) | |  | | |__) | | |    | (___ | |       /  \  |  \| |  \| | |__  | |__) |
+ |  ___/| |  | |  _  /  | |     \___ \| |      / /\ \ | . ` | . ` |  __| |  _  / 
+ | |    | |__| | | \ \  | |     ____) | |____ / ____ \| |\  | |\  | |____| | \ \ 
+ |_|     \____/|_|  \_\ |_|    |_____/ \_____/_/    \_\_| \_|_| \_|______|_|  \_\
+                                                                                 
+                                                                                 
+''')
 
-print "1 - Scan Ports"
-print "2 - Get Target IP\n"
+print (
+'''
+ ____________________________
+         Port Scanner	     
+ ____________________________
+ 	      Ashkan_Z9	     
+ ____________________________
+      Telegram: @Ashkan_Z9   
+ ____________________________
+
+''')
+
+print "1 - SCAN PORTS"
+print "2 - GET TARGET IP"
+print "3 - EXIT\n"
 n = raw_input("=> ")
 
 if n == '1':
@@ -102,6 +141,9 @@ if n == '1':
 
 elif n ==  '2':
     GetIp()
+
+elif n == '3':
+	sys.exit()
 
 else:
     print "\nUnknown Command!"
